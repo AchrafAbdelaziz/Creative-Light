8a482d76bbf024d6c1b9c1c99a72aaebcea7b282
AchrafAbdelaziz <47815491+AchrafAbdelaziz@users.noreply.github.com>
2020-06-15 15:02:13 +0200

    core updaate

---
+++ core/panierC.php
@@ -0,0 +1,126 @@
+<?PHP
+include "../config.php";
+class panierC {
+function afficherpanier ($panier){
+		echo "id: ".$panier->getid()."<br>";
+		echo "date: ".$panier->getdate()."<br>";
+		echo "produit: ".$panier->getproduit()."<br>";
+	}
+	function ajouterpanier($panier){
+		$sql="insert into panier (id,date,produit) values (:id, :date,:produit)";
+		$db = config::getConnexion();
+		try{
+        $req=$db->prepare($sql);
+		
+        $id=$panier->getid();
+        $date=$panier->getdate();
+        $produit=$panier->getproduit();
+		$req->bindValue(':id',$id);
+		$req->bindValue(':date',$date);
+		$req->bindValue(':produit',$produit);
+		
+            $req->execute();
+           
+        }
+        catch (Exception $e){
+            echo 'Erreur: '.$e->getMessage();
+        }
+		
+	}
+	
+	function afficherpaniers(){
+		//$sql="SElECT * From panier e inner join formationphp.panier a on e.id= a.id";
+		$sql="SElECT * From panier";
+		$db = config::getConnexion();
+		try{
+		$liste=$db->query($sql);
+		return $liste;
+		}
+        catch (Exception $e){
+            die('Erreur: '.$e->getMessage());
+        }	
+	}
+	function supprimerpanier($id){
+		$sql="DELETE FROM panier where id= :id";
+		$db = config::getConnexion();
+        $req=$db->prepare($sql);
+		$req->bindValue(':id',$id);
+		try{
+            $req->execute();
+           // header('Location: index.php');
+        }
+        catch (Exception $e){
+            die('Erreur: '.$e->getMessage());
+        }
+	}
+	function modifierpanier($panier,$id){
+		$sql="UPDATE panier SET id=:idn, date=:date, produit=:produit WHERE id=:id";
+		
+		$db = config::getConnexion();
+		//$db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+try{		
+        $req=$db->prepare($sql);
+		$idn=$panier->getid();
+        $date=$panier->getdate();
+        $produit=$panier->getproduit();
+        
+		$datas = array(':idn'=>$idn, ':id'=>$id, ':date'=>$date,':produit'=>$produit);
+		$req->bindValue(':idn',$idn);
+		$req->bindValue(':id',$id);
+		$req->bindValue(':date',$date);
+		$req->bindValue(':produit',$produit);
+		
+		
+            $s=$req->execute();
+			
+           // header('Location: index.php');
+        }
+        catch (Exception $e){
+            echo " Erreur ! ".$e->getMessage();
+   echo " Les datas : " ;
+  print_r($datas);
+        }
+		
+	}
+	function recupererpanier($id){
+		$sql="SELECT * from panier where id=$id";
+		$db = config::getConnexion();
+		try{
+		$liste=$db->query($sql);
+		return $liste;
+		}
+        catch (Exception $e){
+            die('Erreur: '.$e->getMessage());
+        }
+	}
+	
+function recherche($produit){
+		$sql="SELECT * from panier where produit like '%$produit%'";
+		$db = config::getConnexion();
+		try{
+		$liste=$db->query($sql);
+		return $liste;
+		}
+        catch (Exception $e){
+            die('Erreur: '.$e->getMessage());
+        }
+	}
+
+function trierpanier(){
+	$sql="SELECT * From panier ORDER by date desc";
+	$db = config::getConnexion();
+	try{
+		$liste=$db->query($sql);
+		return $liste;
+	}
+	catch(Exception $e){
+		die ('Erreur: '.$e->getMessage());
+	}
+
+	}	
+
+
+
+
+}
+?>
